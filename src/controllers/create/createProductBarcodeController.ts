d03bbbf704e9bb57ef630cae7a2744111378a094
LucasHenrique-Silva <Lucash.s.f74@gmail.com>
2026-01-23 09:29:45 -0300

    auth fo barcode

--- src/controllers/create/createProductBarcodeController.ts
+++ src/controllers/create/createProductBarcodeController.ts
@@ -1,11 +1,16 @@
-import { Request, Response } from "express";
+import { Response } from "express";
 import { createProductBarcodeService } from "../../services/create/createProductBarcodeService";
+import { AuthenticatedRequest } from "../../types/expressRequest";
+import { UserRole } from "@prisma/client";
+import { AppError } from "../../errors/AppError";
     
 
 export class CreateProductBarcodeController {
-    async handle(req: Request, res: Response) {
+    async handle(req: AuthenticatedRequest, res: Response) {
         const data = req.body;
-        
+        if(req.role == UserRole.FUNCIONARIO){
+            throw new AppError("Acesso negado.")
+        }
         const service = new createProductBarcodeService();
         
 
--- src/routes/create/createProductBarcode.routes.ts
+++ src/routes/create/createProductBarcode.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { CreateProductBarcodeController } from "../../controllers/create/createProductBarcodeController";
+import { authMiddleware } from "../../middleware/authMiddleware";
 
 export const createProductRoutesBarcode = Router();
 const controller = new CreateProductBarcodeController();
 
-createProductRoutesBarcode.post("/productBarcode", controller.handle.bind(controller));
+createProductRoutesBarcode.post("/productBarcode", authMiddleware,  (req, res) => controller.handle(req as any, res));
